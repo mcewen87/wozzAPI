0733f6c9ca78b7033b3d1c6ef5b511eeb0e4306a
Jon McEwen <>
2019-12-21 19:13:56 -0600

    cors

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const whitelist = [
 const options = {
   // http://localhost:8000
   // https://wozzapp.netlify.com
-  origin: "https://wozzapp.netlify.com/",
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: [
     "Content-Type",
